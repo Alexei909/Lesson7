04e2c4f1b2831c300308bb0c717a2ab4dd628936
Alexei Ilivanov <vegetablse912@gmail.com>
2023-05-23 21:36:21 +0500

    Урок 10

---
+++ models/core/factories.py
@@ -0,0 +1,13 @@
+import factory
+from faker import Factory
+
+from core import models
+
+factory_ru = Factory.create("ru-Ru")
+
+
+class PhoneFactory(factory.django.DjangoModelFactory):
+    name = factory_ru.phone_name()
+
+    class Meta:
+        model = models.Phone
